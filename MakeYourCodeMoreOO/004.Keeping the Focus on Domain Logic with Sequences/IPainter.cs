c41980dfc98e90e47192c66d7cdaf9e8cbeb9418
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2017-12-26 13:27:18 +0200

    01.Understanding the Problem

---
+++ MakeYourCodeMoreOO/004.Keeping the Focus on Domain Logic with Sequences/IPainter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004.Keeping_the_Focus_on_Domain_Logic_with_Sequences
+{
+    interface IPainter
+    {
+    }
+}
---
+++ MakeYourCodeMoreOO/004.Keeping the Focus on Domain Logic with Sequences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004.Keeping_the_Focus_on_Domain_Logic_with_Sequences
+{
+    class Program
+    {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            // normal solution to select the cheapest painter
+            // this is bad coding style
+            // because it is difficult to understand what this code is doing.
+            // because it need big time to prove that it works correctly.
+            // there is 3 levels of branching
+
+            //our target in this module to remove this loop with something more clean
+            double bestPrice = 0;
+            IPainter cheapest = null;
+            foreach (IPainter painter in painters)
+            {
+                if (painter.IsAvailable)
+                {
+                    double price = painter.EstimateCompensation(sqMeters);
+                    if (cheapest == null || price < bestPrice)
+                    {
+                        cheapest = painter;
+                    }
+                }
+            }
+            return cheapest;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
